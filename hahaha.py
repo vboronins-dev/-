f37aea90c6d352d0b1e5bcd39d6eed67a977a9dc
vboronins-dev <vboronins@gmail.com>
2025-12-11 15:05:11 +0300

    okey

--- hahaha.py
+++ hahaha.py
@@ -2,4 +2,7 @@ class pers:
     def _init_(self, name, hp):
         self.name = name
         self.hp = hp
-    def get_name()
+    def get_name (self):
+        return self.name
+    def get_hp (self):
+        return self.hp
