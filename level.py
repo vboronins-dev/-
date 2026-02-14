767731d1b41c95152c4238e38ff07aaf658dfd16
vboronins-dev <vboronins@gmail.com>
2026-02-14 14:42:32 +0300

    Okeyyyy!

---
+++ level.py
@@ -0,0 +1,30 @@
+DIFFICULTY_SETTINGS = {
+    "easy": {
+        "player_speed": 30,
+        "enemy_speed": 3,
+        "enemy_spawn_time": 2000,
+        "enemy_hp": 30,
+        "enemy_damage": 5
+    },
+    "normal": {
+        "player_speed": 25,
+        "enemy_speed": 5,
+        "enemy_spawn_time": 1000,
+        "enemy_hp": 50,
+        "enemy_damage": 10
+    },
+    "hard": {
+        "player_speed": 20,
+        "enemy_speed": 7,
+        "enemy_spawn_time": 700,
+        "enemy_hp": 70,
+        "enemy_damage": 15
+    },
+    "hardcore": {
+        "player_speed": 15,
+        "enemy_speed": 10,
+        "enemy_spawn_time": 400,
+        "enemy_hp": 100,
+        "enemy_damage": 20
+    }
+}
